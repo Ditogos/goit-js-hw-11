248e00cf2f10709e9b012e7a459a270a2528b5e0
Artem <krylov.artem.1995@gmail.com>
2023-10-25 19:42:54 +0200

    Final

---
+++ src/js/SearchService.js
@@ -0,0 +1,31 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+export { SearchService };
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '40248939-f712c7f983066a304da5b3485';
+
+async function SearchService(currentPage, searchQuery) {
+  const parameters = new URLSearchParams({
+    key: API_KEY,
+    image_type: `photo`,
+    orientation: `horizontal`,
+    safesearch: `true`,
+    per_page: '40',
+    q: searchQuery,
+    page: currentPage,
+  });
+
+  try {
+    const resp = await fetch(`${BASE_URL}?${parameters}`);
+    if (!resp.ok) {
+      throw new Error(`HTTP Error! Status: ${resp.status}`);
+    }
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    Notify.failure(
+      `Unfortunately, there are no images matching your request. Please try again.`
+    );
+    throw error;
+  }
+}
